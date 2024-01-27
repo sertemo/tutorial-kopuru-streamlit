a0c900c138082e7d21debbc475f5f6b442cfd91d
sertemo <tejedor.moreno@gmail.com>
2024-01-27 17:34:41 +0100

    añadido un poco mas. Queda meterse con el modelo

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
+# Copyright 2024 Sergio Tejedor Moreno
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # Librerías internas
 
@@ -13,7 +26,7 @@ st.set_page_config(
 )
 
 with st.sidebar:
-    st.write(':gray[Modelo]')
+    st.write('© 2024 STM')
 
 st.title('Reconocimiento de dígitos')
 st.subheader('Una App para Kopuru')
@@ -23,7 +36,7 @@ st.write('''Con esta app serás capaz de evaluar un modelo convolucional entrena
             y súbela. Pulsa sobre el botón **predecir** y comprueba si el modelo ha sido
             capaz de averiguar el dígito que habías dibujado.''')
 
-tab1, tab2, tab3 = st.tabs(['Cargar Imagen', 'Ver dígito', 'Ver Predicciones'])
+tab1, tab2, tab3, tab4 = st.tabs(['Cargar imagen', 'Ver dígito', 'Ver prediccion', 'Ver historial'])
 
 with tab1:
     st.write('''Carga tu imagen con el dígito dibujado. 
@@ -31,3 +44,42 @@ with tab1:
         dibujado en blanco sobre color negro.''')
     
     imagen = st.file_uploader('Sube tu dígito', type=["png","tif","jpg","bmp","jpeg"])
+
+    # TODO validaciones
+
+    if imagen is not None: #TODO and validaciones
+        predecir = st.button("Predecir")
+
+        if predecir:
+            # TODO barra de progreso ?
+            # TODO lanzar predicción
+            pass
+
+with tab2:
+    # TODO plotear con matplotlib imshow el dígito
+    pass
+
+with tab3:
+    col1, col2 = st.columns(2)
+    with col1:
+        st.metric("Predicción del modelo", value=2)
+    with col2:
+        st.metric("Confianza de la predicción", value=88)
+    
+    st.write("¿ Ha acertado el modelo ?")
+    st.number_input("Marca el dígito que habías dibujado", min_value=0, max_value=9)
+
+    guardar_pred = st.button("Guardar valores", help='Añade los valores al historial')
+    if guardar_pred:
+        # TODO No guardar si nombre de archivo existe en historial
+        pass
+
+with tab4:
+    # TODO Mostrar acumulado de las predicciones: +1 acierto -1 fallo
+    # TODO Mostrar porcentaje de aciertos
+    pass
+
+    
+
+st.session_state
+    
